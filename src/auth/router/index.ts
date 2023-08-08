db93426b8b56947b3e919d95c0f07bdc61f66947
tdadadavid <davidtofunmidada@gmail.com>
2023-08-08 10:21:34 +0100

    export listeners

--- src/auth/router/index.ts
+++ src/auth/router/index.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { authRateLimiter, controllerHandler } from "../../core";
 import { signIn, signUp, refreshToken } from "../services";
+import { refreshTokenSchema, signInSchema, signUpSchema } from "./schemas";
 
 export const authRouter = Router();
 
 
 authRouter
     .use(authRateLimiter)
-    .post("/signup", controllerHandler.handle(signUp))
-    .post("/signIn", controllerHandler.handle(signIn))
-    .post("/refresh", controllerHandler.handle(refreshToken))
+    .post("/signup", controllerHandler.handle(signUp, signUpSchema))
+    .post("/signIn", controllerHandler.handle(signIn, signInSchema))
+    .post("/refresh", controllerHandler.handle(refreshToken, refreshTokenSchema))
--- src/auth/router/schemas/sign.in.schema.ts
+++ src/auth/router/schemas/sign.in.schema.ts
@@ -1,10 +1,10 @@
-import { ValidationSchema } from "../../../core";
-
 import * as Joi from "joi";
+
+import { ValidationSchema } from "../../../core";
 
 export const signInSchema: ValidationSchema = {
     inputSchema: Joi.object({
         email: Joi.string().email().required(),
-        password: Joi.string().min(8).max(12).required(),
+        password: Joi.string().min(8).max(18).required(),
     })
 }
