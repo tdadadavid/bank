ac2f6ddcb50d83046632288ea952f5482505fd18
tdadadavid <davidtofunmidada@gmail.com>
2023-08-11 23:35:27 +0100

    remove comment
    Signed-off-by: tdadadavid <davidtofunmidada@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { startApp } from "./app";
 import { gracefullyShutdown, initializeDbConnection } from "./core";
 
+//# begin work on testing and kubernetes integration
 
 initializeDbConnection()
     .then(startApp)
