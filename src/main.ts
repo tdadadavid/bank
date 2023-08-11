7d663d860dcfa48911ed6765b2d28e4420ce9251
tdadadavid <davidtofunmidada@gmail.com>
2023-08-11 23:30:06 +0100

    remove comment

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { startApp } from "./app";
 import { gracefullyShutdown, initializeDbConnection } from "./core";
 
-//TODO: work on the docker, then setup kubernetes for it.
 
 initializeDbConnection()
     .then(startApp)
